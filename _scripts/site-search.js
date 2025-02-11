84944c6bcddfb3dc92e983454292d4e85b34fd79
server2-user <server2@netpd.lan>
2025-02-11 06:38:12 +0000

    Migrate site search to DuckDuckGo

--- _scripts/site-search.js
+++ _scripts/site-search.js
@@ -6,9 +6,8 @@
   // when user submits site search form/box
   window.onSiteSearchSubmit = (event) => {
     event.preventDefault();
-    const google = "https://www.google.com/search?q=site:";
     const site = window.location.origin;
     const query = event.target.elements.query.value;
-    window.location = google + site + " " + query;
+    window.location = "https://duckduckgo.com/?q=site:" + site + " " + query;
   };
 }
